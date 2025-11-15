d9cc26d78c1fdc031143728b0aece5155bff635f
Souza <lucas11souza97@gmail.com>
2025-11-14 20:51:18 -0400

    feat: created project

---
+++ README copy.md
@@ -0,0 +1,8 @@
+# BCFPP = Basic Components For Python Projects
+
+
+### This project aims to share components and features that would be replicated in most Python projects.
+
+### generete lib: python setup.py sdist 
+
+### send to pypi: twine upload dist/*
---
+++ test.py
@@ -0,0 +1,9 @@
+# from basic_components_fpp.exception import UnauthorizedAPI
+# from basic_components_fpp.request import Request
+
+
+# print(Request(url="test").url)
+# print(UnauthorizedAPI.message)
+# print(UnauthorizedAPI.status)
+# print(UnauthorizedAPI.status_code)
+# raise UnauthorizedAPI()
